2ea0fbda7755c809b0c7881b674ee88664e2f349
guneykilicel <kilicelguney@gmail.com>
2023-06-19 16:44:19 +0300

    ngModal Cift Tarafli Bind

---
+++ src/app/model.ts
@@ -0,0 +1,24 @@
+export class Model {
+    user: string;
+    items: TodoItem[];
+
+    constructor() {
+        this.user = "Çınar";
+        this.items = [
+            new TodoItem ("Spor", true),
+            new TodoItem ("Kahvaltı", false),
+            new TodoItem ("Kitap Okumak", false),
+            new TodoItem ("Sinema", false),
+        ]
+    }
+}
+
+export class TodoItem {
+    description: string;
+    action: boolean;
+
+    constructor (description: string, action: boolean) {
+        this.description = description;
+        this.action = action;
+    }
+}
